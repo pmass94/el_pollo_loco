aee22a8e84df784fd29e470de3f3311cdf0d9b06
Peter Mass <mass.peter@web.de>
2021-07-26 15:12:36 +0200

    version update

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,8 +1,9 @@
-class Chicken extends MovableObject{
-y= 360;
-height = 70;
-width = 80;
-    constructor(){
+class Chicken extends MovableObject {
+    y = 360;
+    height = 70;
+    width = 80;
+    y = 360
+    constructor() {
         super().loadImage('img/3.Secuencias_Enemy_básico/Versión_Gallinita (estas salen por orden de la gallina gigantona)/1.Ga_paso_derecho.png')
 
         this.x = 200 + Math.random() * 500; // Zahl zwischen 200 und 700
--- models/cloud.class.js
+++ models/cloud.class.js
@@ -7,7 +7,14 @@ class Cloud extends MovableObject {
         super().loadImage('img/5.Fondo/Capas/4.nubes/1.png');
 
         this.x = Math.random() * 500; // Zahl zwischen 200 und 700
+        this.animate();
+    }
 
+    animate() {
+        setInterval(() => {
+            this.x -= 0.2;
+        },1000 / 60);
+        
     }
 
 }
