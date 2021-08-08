b9dc862fd31b69422f5bc1995f0e152c56cd6865
Peter Mass <mass.peter@web.de>
2021-08-08 15:03:41 +0200

    version update

--- models/character.class.js
+++ models/character.class.js
@@ -45,11 +45,13 @@ class Character extends MovableObject {
             if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.moveRight();
                 this.walking_sound.play();
+                this.otherDirection = false;
             }
 
             if (this.world.keyboard.LEFT && this.x > 0) {
                 this.moveLeft();
                 this.walking_sound.play();
+                this.otherDirection = true;
             }
 
             if (this.world.keyboard.SPACE && !this.isAboveGround()) {
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -30,6 +30,29 @@ class MovableObject {
         this.img.src = path;
     }
 
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+    drawFrame(ctx) {
+
+        if (this instanceof Character || this instanceof Chicken) {
+            ctx.beginPath();
+            ctx.lineWidth = "5";
+            ctx.strokeStyle = "blue";
+            ctx.rect(this.x, this.y, this.width, this.height);
+            ctx.stroke();
+        }
+    }
+
+    // character.isColliding(chicken)
+    isColliding(mo) {
+        return this.x + this.width > mo.x &&
+            this.y + this.height > mo.y &&
+            this.x < mo.x &&
+            this.y < mo.y + mo.height
+    }
+
     /**
      * 
      * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...] 
@@ -51,13 +74,13 @@ class MovableObject {
 
     moveRight() {
         this.x += this.speed;
-        this.otherDirection = false;
+
 
     }
 
     moveLeft() {
         this.x -= this.speed;
-        this.otherDirection = true;
+
     }
 
     jump() {
