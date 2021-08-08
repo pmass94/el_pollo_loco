860622cf99eda7d51fb1464714b38dca35993adc
Peter Mass <mass.peter@web.de>
2021-08-08 16:56:36 +0200

    version update

--- models/world.class.js
+++ models/world.class.js
@@ -23,7 +23,8 @@ class World {
         setInterval(() => {
             this.level.enemies.forEach((enemy) => {
                if(this.character.isColliding(enemy)){
-
+                   this.character.hit();
+                    console.log('Collision with Character', this.character.energy)
                }
             });
         }, 200);
