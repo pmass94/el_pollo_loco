5e677b7607099c7113a078c2b9bf773ef56e5ccf
Peter Mass <mass.peter@web.de>
2021-08-24 19:53:02 +0200

    version update

--- models/coin-bar.class.js
+++ models/coin-bar.class.js
@@ -1,12 +1,30 @@
-class CoinBar extends StatusBar {
+class CoinBar extends DrawableObject {
  
-    
+    IMAGES = [
+        'img/7.Marcadores/Barra/Marcador moneda/azul/0_.png',
+        'img/7.Marcadores/Barra/Marcador moneda/azul/20_.png',
+        'img/7.Marcadores/Barra/Marcador moneda/azul/40_.png',
+        'img/7.Marcadores/Barra/Marcador moneda/azul/60_.png',
+        'img/7.Marcadores/Barra/Marcador moneda/azul/80_.png',
+        'img/7.Marcadores/Barra/Marcador moneda/azul/100_.png',
+    ];
+
+    percentage = 100;
     
     constructor() {
         super().loadImage('img/7.Marcadores/Barra/Marcador moneda/azul/0_.png');
         this.loadImages(this.IMAGES);
         this.x = 250;
+        this.y = 0;
+        this.width = 200;
+        this.height = 60;
         this.setPercentage(0);
+    }
+
+    setPercentage(percentage) {
+        this.percentage = percentage;
+        let path = this.IMAGES[this.resolveImageIndex()];
+        this.img = this.imageCache[path];
     }
 
     resolveImageIndex() {
@@ -25,12 +43,6 @@ class CoinBar extends StatusBar {
         }
     }
 
-    IMAGES = [
-        'img/7.Marcadores/Barra/Marcador moneda/azul/0_.png',
-        'img/7.Marcadores/Barra/Marcador moneda/azul/20_.png',
-        'img/7.Marcadores/Barra/Marcador moneda/azul/40_.png',
-        'img/7.Marcadores/Barra/Marcador moneda/azul/60_.png',
-        'img/7.Marcadores/Barra/Marcador moneda/azul/80_.png',
-        'img/7.Marcadores/Barra/Marcador moneda/azul/100_.png',
-    ];
+
 }
+
